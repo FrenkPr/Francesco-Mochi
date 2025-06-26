b269eeac34db5996134f6825dc134c2cf13c0c2c
FrenkPr <francescomochi2002@gmail.com>
2025-06-26 19:03:05 +0200

    26-06 parte 2
    ArrayList

---
+++ 26-06/ArrayListProva.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ArrayListProva
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> numeri = new ArrayList<>();
+
+        numeri.add(10);
+        numeri.add(20);
+        numeri.add(30);
+    }
+}
