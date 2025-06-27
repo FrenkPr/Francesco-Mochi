75301f9002c092c58761a332a2e79a3b70910725
FrenkPr <francescomochi2002@gmail.com>
2025-06-27 10:12:27 +0200

    24-06 Implicit casting

---
+++ 24-06/ImplicitCasting.java
@@ -0,0 +1,11 @@
+public class ImplicitCasting
+{
+    public static void main(String[] args)
+	{
+        int myInt = 9;
+		double myDouble = myInt; //automatic casting: int to double
+
+        System.out.println(myInt); //Outputs 9
+        System.out.println(myDouble); //Outputs 9.0
+	}
+}
